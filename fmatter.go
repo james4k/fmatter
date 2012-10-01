90c2f631cb0057f22bd0afc22b58a39128e00232
James Gray <james@james4k.com>
2012-09-30 22:44:56 -0500

    Added initial files.

---
+++ fmatter.go
@@ -0,0 +1,75 @@
+// fmatter is a simple YAML Front Matter parser, using the
+// launchpad.net/goyaml package.
+package fmatter
+
+import (
+	"bytes"
+	"io"
+	"io/ioutil"
+	"launchpad.net/goyaml"
+	"strings"
+	"unicode"
+)
+
+// ReadFile read an entire file into memory, and calls Read which parses the front matter data and returns the remaining file contents.
+func ReadFile(filename string, frontmatter interface{}) (content []byte, err error) {
+	data, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return Read(data, frontmatter)
+}
+
+// Read detects and parses the front matter data, and returns the remaining contents. If no front matter is found, the entire file contents are returned.
+// For details on the frontmatter output, please see the launchpad.net/goyaml package.
+func Read(data []byte, frontmatter interface{}) (content []byte, err error) {
+	r := bytes.NewBuffer(data)
+
+	// eat away starting whitespace
+	var ch rune = ' '
+	for unicode.IsSpace(ch) {
+		ch, _, err = r.ReadRune()
+		if err != nil {
+			// file is just whitespace
+			return []byte{}, nil
+		}
+	}
+	r.UnreadRune()
+
+	// check if first line is ---
+	line, err := r.ReadString('\n')
+	if err != nil && err != io.EOF {
+		return nil, err
+	}
+
+	if strings.TrimSpace(line) != "---" {
+		// no front matter, just content
+		return data, nil
+	}
+
+	yamlStart := len(data) - r.Len()
+	yamlEnd := yamlStart
+
+	for {
+		line, err = r.ReadString('\n')
+		if err != nil {
+			if err == io.EOF {
+				return data, nil
+			}
+			return nil, err
+		}
+
+		if strings.TrimSpace(line) == "---" {
+			yamlEnd = len(data) - r.Len()
+			break
+		}
+	}
+
+	err = goyaml.Unmarshal(data[yamlStart:yamlEnd], frontmatter)
+	if err != nil {
+		return nil, err
+	}
+	content = data[yamlEnd:]
+	err = nil
+	return
+}
---
+++ fmatter_test.go
@@ -0,0 +1,44 @@
+package fmatter
+
+import (
+	"bytes"
+	"testing"
+)
+
+// TODO: test a few expected errors
+
+type testItem struct {
+	data            []byte
+	expectedContent []byte
+}
+
+var testItems = []testItem{
+	testItem{[]byte(`---
+frontmatter: simple
+---
+content`),
+		[]byte(`content`)},
+	testItem{[]byte(`  content`),
+		[]byte(`  content`)},
+	testItem{[]byte(`---
+content`),
+		[]byte(`---
+content`)},
+}
+
+func TestItems(t *testing.T) {
+	frontmatter := make(map[string]interface{})
+
+	for _, item := range testItems {
+		content, err := Read(item.data, frontmatter)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if bytes.Compare(content, item.expectedContent) != 0 {
+			t.Fatalf("unexpected content:\n%v\nvs.\n%v",
+				string(content), string(item.expectedContent))
+		}
+	}
+}
+
